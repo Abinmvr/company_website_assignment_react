cc22a27fb4c7f5a0ca5b83b26cf42302f61ed3ba
Abinmvr <abinvmvrcalicut@gmail.com>
2022-09-29 17:39:07 +0530

    intercepter added

---
+++ src/API/homeApI.tsx
@@ -0,0 +1,5 @@
+import axios from 'axios'
+// import axiosInstance from './axiosInstance';
+import { AppDispatch } from '../redux/store'
+// import {setInsight,setError,setLoad} from '../redux/insightRedux/insightAction';
+// import { setAchieve,setError,setLoad } from "../redux/achieveRedux/achieveAction";
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
+
+axios.interceptors.request.use((request:any)=>{
+    const token:any =localStorage.getItem('token');
+	request.headers.authorization=token;
+	return request;
+});
+axios.interceptors.response.use((response:any)=>{
+	return response;
+});
+
 ReactDOM.render(
 <React.StrictMode>
 <App />
